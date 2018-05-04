f5062182bd92d577275e0436b7197ea14db80d33
denisenricohasyim93 <denisenricohasyim93@gmail.com>
2018-05-05 06:24:12 +0700

    another commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Row, Col, Grid, PageHeader, Jumbotron, Button } from 'react-bootstrap';
+import { Grid, PageHeader, Jumbotron, Button } from 'react-bootstrap';
 import Center from 'react-center';
 import InstagramEmbed from 'react-instagram-embed';
 import StickyFooter from 'react-sticky-footer';
 import GoogleMapReact from 'google-map-react';
+import Sticky from 'react-stickynode';
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <PageHeader style={{marginTop: 25, marginLeft: 25}}>
-          #kebunkode
-        </PageHeader>;
+        <Sticky top='#header' bottomBoundary='#content'>
+          <div style={{backgroundColor: 'blue', color: 'yellow'}}>
+            #kebunkode
+          </div>
+        </Sticky>
         <Center style={{height: 700, backgroundColor: 'black', flexDirection: 'column'}}>
           <div style={{color: 'white'}}>#kebunkode</div>
         </Center>
@@ -21,17 +25,17 @@ class App extends Component {
           <div style={{color: 'white'}}>#kebunkode</div>
         </Center>
         <StickyFooter
-            bottomThreshold={50}
-            normalStyles={{
+          bottomThreshold={50}
+          normalStyles={{
             backgroundColor: "green",
             padding: "2rem"
-            }}
-            stickyStyles={{
+          }}
+          stickyStyles={{
             backgroundColor: "green",
             padding: "2rem"
-            }}
-        >
-            copyright #kebunkode
+          }}
+          >
+          copyright #kebunkode
         </StickyFooter>
       </div>
     );
